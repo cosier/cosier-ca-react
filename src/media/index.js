7f13fefbe8faff07f5cf092eb0949f8e275fb945
Bailey Cosier <bailey.cosier@gmail.com>
2017-09-11 17:53:16 +0700

    Use webpack for managing media files via code imports

--- src/media/index.js
+++ src/media/index.js
@@ -1,3 +1,36 @@
-import LogoMini from '../../public/images/logo_mini.png';
+import M1 from '../../public/images/intro/m_1.png';
+import M2 from '../../public/images/intro/m_2.png';
+import M3 from '../../public/images/intro/m_3.png';
+import M4 from '../../public/images/intro/m_4.png';
+import M5 from '../../public/images/intro/m_5.png';
+import M6 from '../../public/images/intro/m_6.png';
+import M7 from '../../public/images/intro/m_7.png';
+import M8 from '../../public/images/intro/m_8.png';
+import M9 from '../../public/images/intro/m_9.png';
+import M10 from '../../public/images/intro/m_10.png';
 
-export {LogoMini};
+import C1 from '../../public/images/intro/cloud_1.png';
+import C2 from '../../public/images/intro/cloud_2.png';
+import C3 from '../../public/images/intro/cloud_3.png';
+import C4 from '../../public/images/intro/cloud_4.png';
+import C5 from '../../public/images/intro/cloud_4.png';
+
+import R1 from '../../public/images/intro/m_rock_1.png';
+import R2 from '../../public/images/intro/m_rock_2.png';
+import R3 from '../../public/images/intro/m_rock_3.png';
+import R4 from '../../public/images/intro/m_rock_4.png';
+import R5 from '../../public/images/intro/m_rock_5.png';
+
+const Mobile = {
+    M1, M2, M3, M4, M5, M6, M7, M8, M9, M10
+}
+
+const Clouds = {
+    C1, C2, C3, C4, C5
+}
+
+const Rocks = {
+    R1, R2, R3, R4, R5
+}
+
+export { Mobile, Clouds, Rocks}
